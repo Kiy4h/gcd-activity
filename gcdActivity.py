91a6837103013a91201e79db29007b98d059bd0e
Zakiyah <z4kiy4h.h@gmail.com>
2020-01-09 10:33:38 +0700

    Port to GTK 3

--- gcdActivity.py
+++ gcdActivity.py
@@ -1,20 +1,26 @@
 from gettext import gettext as _
 
 import sys
-import gtk
 import pygame
 
-import sugar.activity.activity
-import sugar.graphics.toolbutton
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
-sys.path.append('..') # Import sugargame package from top directory.
+from sugar3.activity import activity
+from sugar3.graphics.toolbutton import ToolButton
+from sugar3.graphics.toolbarbox import ToolbarBox
+
+sys.path.append('..')  # Import sugargame package from top directory.
 import sugargame.canvas
 
 import mcd
-class gcdActivity(sugar.activity.activity.Activity):
+
+
+class gcdActivity(activity.Activity):
     def __init__(self, handle):
         super(gcdActivity, self).__init__(handle)
-        
+
         self.paused = False
 
         # Create the game instance.
@@ -28,40 +34,40 @@ class gcdActivity(sugar.activity.activity.Activity):
 
         # Note that set_canvas implicitly calls read_file when resuming from the Journal.
         self.set_canvas(self._pygamecanvas)
-        
+
         # Start the game running (self.game.run is called when the activity constructor returns).
         self._pygamecanvas.run_pygame(mcd.run())
-        
-    def build_toolbar(self):        
-        stop_play = sugar.graphics.toolbutton.ToolButton('media-playback-stop')
+
+    def build_toolbar(self):
+        stop_play = ToolButton('media-playback-stop')
         stop_play.set_tooltip(_("Stop"))
         stop_play.set_accelerator(_('<ctrl>space'))
         stop_play.connect('clicked', self._stop_play_cb)
 
-        toolbar = gtk.Toolbar()
+        toolbar = Gtk.Toolbar()
         #toolbar.insert(stop_play, 0)
-        
-        toolbox = sugar.activity.activity.ActivityToolbox(self)
+
+        toolbox = ToolbarBox()
         toolbox.add_toolbar(_("Pygame"), toolbar)
-        
+
         toolbox.show_all()
-        self.set_toolbox(toolbox)
+        self.set_toolbar_box(toolbox)
 
     def _stop_play_cb(self, button):
         pass
         # Pause or unpause the game.
         #self.paused = not self.paused
-        #self.game.set_paused(self.paused)
+        # self.game.set_paused(self.paused)
         # Update the button to show the next action.
-        #if self.paused:
+        # if self.paused:
         #    button.set_icon('media-playback-start')
         #    button.set_tooltip(_("Start"))
-        #else:
+        # else:
         #    button.set_icon('media-playback-stop')
         #    button.set_tooltip(_("Stop"))
 
     def read_file(self, file_path):
         mcd.read_file(file_path)
-        
+
     def write_file(self, file_path):
         mcd.write_file(file_path)
